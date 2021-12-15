fd36abc5a3aabcf68cb3f89da00fb0a2d5fd42fe
Kilian De Bock <Stualyttle@users.noreply.github.com>
2021-12-15 19:54:53 +0100

    21.50.5.3: Added data for pgm.json + its images, started working on services.js (main layout)

---
+++ static/js/services.js
@@ -0,0 +1,25 @@
+function GitHubApi() {
+  this.getSearchUsers = async () => {
+  };
+  this.getReposOfUsers = async () => {
+  };
+  this.getFollowersOfUsers = async () => {
+  };
+  this.getSubscribersOfUsers = async () => {
+  };
+}
+
+function UsersApi() {
+  this.getUsers = async () => {
+  };
+}
+
+function WeatherApi() {
+  this.getCurrentWeather = async () => {
+  };
+}
+
+function GhentOpenDataApi() {
+  this.getCovidPositiveCases = async () => {
+  };
+}
